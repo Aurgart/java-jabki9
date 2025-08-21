da3bdff1f7da7302953564743b642c7deacbb549
Aurgart <ilyalemeshew@gmail.com>
2025-08-21 12:55:19 +0300

    ДЗ 9

---
+++ src/MainTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    public void areAnagramsTest() {
+        assertEquals(true, Main.anagramOrNot("test", "sett"));
+    }
+
+    @Test
+    public void haveDuplicatesTest() {
+        List<Integer> tmlst = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 1, 2, 3));
+        assertTrue(Main.hasDuplicates(tmlst));
+    }
+}
