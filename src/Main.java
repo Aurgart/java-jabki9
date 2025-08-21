4de8f4e584e6e382aefacc76515c119fc68cce54
Aurgart <ilyalemeshew@gmail.com>
2025-08-21 10:07:58 +0300

    Инициализация домашки

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
